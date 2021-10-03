4dcffb1ff0c1423b3e01d1495f59730deecf31c4
hyn_nnn <k5420019@stu.chs.nihon-u.ac.jp>
2021-10-03 18:04:12 +0900

    modoshita

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/aikon.png') {
-    myImage.setAttribute ('src','images/aikon.png');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute ('src','images/aikon.png');
+    myImage.setAttribute ('src','images/firefox-icon.png');
   }
 }
 
@@ -17,12 +17,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('名前を入力してね');
+  let myName = prompt('Please enter your name.');
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML =myName + 'さん、こんにちは！';
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
   }
 }
 
@@ -30,7 +30,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = storedName + 'さん、こんにちは！';
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
